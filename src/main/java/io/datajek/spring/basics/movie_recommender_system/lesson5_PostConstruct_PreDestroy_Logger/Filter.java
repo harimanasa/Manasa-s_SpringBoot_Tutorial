87fac7f52ad26cc303230f1e1c611e3fa225469b
harimanasa <harimanasa17@gmail.com>
2024-09-20 14:09:40 -0700

    Explored
    1. PostConstruct
    2. PreDestroy
    3. Logger

---
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson5_PostConstruct_PreDestroy_Logger/Filter.java
@@ -0,0 +1,6 @@
+package io.datajek.spring.basics.movie_recommender_system.lesson5_PostConstruct_PreDestroy_Logger;
+
+public interface Filter {
+    public String[] getRecommendations(String movie);
+
+}
